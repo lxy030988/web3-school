1a3dd86fc277ca8fa4b1ba8e6b3cb4b7a002121b
lxy030988 <654762937@qq.com>
2025-11-26 11:34:52 +0800

    Add configuration files

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'web3': { primary: '#8B5CF6', secondary: '#EC4899', accent: '#10B981' }
+      }
+    }
+  },
+  plugins: []
+}
